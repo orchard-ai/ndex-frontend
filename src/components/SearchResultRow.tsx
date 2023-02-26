9003fec4f5c0ea9a1ecb546796b11e6fe22ebd9e
Matthew <matthewdukepan@gmail.com>
2023-02-25 23:22:57 -0500

    memoized results for performance

--- src/components/SearchResultRow.tsx
+++ src/components/SearchResultRow.tsx
@@ -4,6 +4,49 @@ import { Highlight } from "react-instantsearch-dom"
 import { SEARCH_COLOR } from "utils/constants"
 import { Hit } from "utils/types"
 import { Combobox } from "@headlessui/react"
+import { memo } from "react"
+
+export default function SearchResultRow({ hit }: { hit: Hit }) {
+  return (
+    <Combobox.Option
+      key={hit?.id}
+      value={hit.url}
+      className={`flex flex-row justify-start w-full m-0 p-0`}
+    >
+      {({ active }) => <MemoizedInnerRow active={active} hit={hit} />}
+    </Combobox.Option>
+  )
+}
+
+const MemoizedInnerRow = memo(
+  ({ hit, active }: { hit: Hit; active: boolean }) => {
+    return (
+      <div
+        className={`p-2 ${
+          active ? "text-white" : "text-gray-700"
+        } rounded-sm w-[54rem] flex flex-row justify-start ${
+          active ? "bg-indigo-600" : `bg-[${SEARCH_COLOR}]`
+        }`}
+      >
+        <div className="flex flex-col text-left w-full">
+          <Title hit={hit} />
+          <Highlight
+            attribute="contents"
+            hit={hit}
+            tagName="mark"
+            className="p-1"
+          />
+        </div>
+        <div className="min-w-4 max-w-4 ml-auto">
+          <a href={hit?.url} target="_blank" className="w-4 h-full flex">
+            <img src={NewTabIcon} alt="New Tab" />
+          </a>
+        </div>
+      </div>
+    )
+  },
+  (prevProps, nextProps) => prevProps.active === nextProps.active
+)
 
 const Title = ({ hit }: { hit: Hit }) => {
   return (
@@ -18,41 +61,3 @@ const Title = ({ hit }: { hit: Hit }) => {
     </div>
   )
 }
-
-const Content = ({ hit }: { hit: Hit }) => {
-  return (
-    <Highlight attribute="contents" hit={hit} tagName="mark" className="p-1" />
-  )
-}
-
-const SearchResultRow = ({ hit }: { hit: Hit }) => {
-  return (
-    <Combobox.Option
-      key={hit?.id}
-      value={hit.url}
-      className={`flex flex-row justify-start w-full m-0 p-0`}
-    >
-      {({ active }) => (
-        <div
-          className={`p-2 ${
-            active ? "text-white" : "text-gray-700"
-          } rounded-sm w-[54rem] flex flex-row justify-start ${
-            active ? "bg-indigo-600" : `bg-[${SEARCH_COLOR}]`
-          }`}
-        >
-          <div className="flex flex-col text-left w-full">
-            <Title hit={hit} />
-            <Content hit={hit} />
-          </div>
-          <div className="min-w-4 max-w-4 ml-auto">
-            <a href={hit?.url} target="_blank" className="w-4 h-full flex">
-              <img src={NewTabIcon} alt="New Tab" />
-            </a>
-          </div>
-        </div>
-      )}
-    </Combobox.Option>
-  )
-}
-
-export default SearchResultRow
